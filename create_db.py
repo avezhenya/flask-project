4a7b2375a1e977bb7fc31f290198e8bf5fa0ba47
avezhenya <avezhenya@gmail.com>
2014-12-02 00:48:25 +0800

    add real books in DB and style for table

--- create_db.py
+++ create_db.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from models import db, User, Author, Book
 
 db.drop_all()
@@ -6,19 +8,15 @@ db.create_all()
 a = User('admin', 'admin')
 db.session.add(a)
 
-c = Author('Pushkin A.S.')
-db.session.add(c)
-# db.session.commit()
-y = Author.query.get(1).id
+books = [u"100 лет одиночества", u"Колыбель для кошки", u"Прощай оружие",
+     u"Повелитель мух", u"Чапаев и пустота", u"Бойцовский клуб", u"451 градус по Фаренгейту"]
+authors = [u"Габриэль Гарсия Маркес", u"Курт Воннегут", u"Эрнест Хемингуэй",
+     u"Уильям Голдинг", u"Виктор Пелевин", u"Чак Паланик", u"Рэй Дуглас Брэдбери"]
+years = [1967, 1980, 1945, 1954, 1996, 1996, 1953]
 
-b = Book('War and freedom', '1874', y)
-ht = Book('Esenin',         '1827', y)
-
-db.session.add(b)
-db.session.add(ht)
+for i in range(len(years)):
+    db.session.add(Author(authors[i]))
+    y = Author.query.get(i+1).id
+    db.session.add(Book(books[i], years[i], y))
 
 db.session.commit()
-
-print User.query.all()
-print Book.query.all()
-print Author.query.all()
